ba280da04ae265150a793c66e3f1efe4179f6275
Donis <adonisjacobs1994@hotmail.com>
2014-12-16 13:52:22 +0100

    Toont grid van getallen (elk getal is een figuur)

--- src/com/company/PacmanFrame.java
+++ src/com/company/PacmanFrame.java
@@ -7,7 +7,7 @@ import java.awt.*;
  * Created by Sefa on 15-12-2014.
  */
 public class PacmanFrame extends JFrame {
-
+    private Speelbord speelbord = new Speelbord();
     private final int WIDTH = 600;
     private final int HEIGHT = 500;
 
@@ -23,18 +23,13 @@ public class PacmanFrame extends JFrame {
 
         this.addStructure();
         this.addMenu();
+        this.drawInitialPacmanGrid();
 
         this.frame.setVisible(true);
     }
 
     private void addStructure()
     {
-        /*
-        JPanel MainPanel = new JPanel();
-        MainPanel.setLayout(new BorderLayout());
-        MainPanel.setSize(this.WIDTH, this.HEIGHT);
-        MainPanel.setBackground(Color.red); */
-
         this.TopPanel.setPreferredSize(new Dimension(this.WIDTH, 50));
         this.TopPanel.setLayout(new GridLayout(0, 2));
 
@@ -56,5 +51,36 @@ public class PacmanFrame extends JFrame {
         startButton.addActionListener(startListener);
 
         this.TopPanel.add(new JLabel("Score: 00"));
+    }
+
+    private void drawInitialPacmanGrid()
+    {
+        for (int x = 0; x < 5; x++)
+        {
+            for (int y = 0; y < 5; y++)
+            {
+                int positie = speelbord.mapArray[x][y];
+                this.BottomPanel.add(new JLabel("" + positie));
+                if(positie == 0)
+                {
+                    //Toon looppad
+                }
+                else if(positie == 1)
+                {
+                    //Toon muur
+                }
+                else if(positie == 2){
+                    //Toon pacman
+                }
+                else if(positie == 3){
+                    //Toon spookje
+                }
+            }
+        }
+    }
+
+    public void starten(Speelbord speelbord){
+
+
     }
 }

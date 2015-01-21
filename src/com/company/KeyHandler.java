defd66fa5bfa66235680392f6ea2a1a9b664b391
Sefa Yavuz <seff61@gmail.com>
2015-01-21 18:44:31 +0100

    ghost ddoesnt go to pacman his place now

--- src/com/company/KeyHandler.java
+++ src/com/company/KeyHandler.java
@@ -20,24 +20,20 @@ public class KeyHandler implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e)
     {
-        this.ghost = this.gameboard.timerHandler.getSmartGhost(1);
+        //this.ghost = this.gameboard.timerHandler.getSmartGhost(1);
         switch (e.getKeyCode())
         {
             case KeyEvent.VK_UP:
                 this.pacman.move(Direction.UP);
-                ghost.findPacman();
                 break;
             case KeyEvent.VK_RIGHT:
                 this.pacman.move(Direction.RIGHT);
-                ghost.findPacman();
                 break;
             case KeyEvent.VK_LEFT:
                 this.pacman.move(Direction.LEFT);
-                ghost.findPacman();
                 break;
             case KeyEvent.VK_DOWN:
                 this.pacman.move(Direction.DOWN);
-                ghost.findPacman();
                 break;
             case KeyEvent.VK_Z:
                 this.gameboard.startPause();

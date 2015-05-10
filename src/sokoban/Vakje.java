8ea49402a13d4a5758b9d969ba5bf90daf84c574
Jhoogland <J.hoogland92@live.nl>
2015-05-10 13:46:08 +0200

    testen git

---
+++ src/sokoban/Vakje.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sokoban;
+    
+/**
+ *
+ * @author Jhoog_000
+ */
+public abstract class Vakje {
+    private int hoogte;
+    private int breedte;
+    private int x;
+    private int y;
+    
+    public abstract Vakje geefVakje();
+}
